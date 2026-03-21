f3976d0da087dfd1da4eef2530af3af13eb35b7c
Zain <zain.ahmed-4@student.manchester.ac.uk>
2026-03-21 19:41:43 +0000

    trying to stack

--- Zain.py
+++ Zain.py
@@ -1,9 +1,3 @@
-#-----------------------------------------------------------------------------#
-#---------------------Quick Start Guide - QArm Mini --------------------------#
-#-----------------------------------------------------------------------------#
-#-----------------------------------------------------------------------------#
-
-# imports
 import numpy as np
 from pal.products.qarm_mini import QArmMini
 from hal.content.qarm_mini import QArmMiniKeyboardNavigator, \
@@ -11,45 +5,87 @@ from hal.content.qarm_mini import QArmMiniKeyboardNavigator, \
 from pal.utilities.keyboard import QKeyboard
 from pal.utilities.timing   import QTimer
 
-## Section A - Setup
-kbd         = QKeyboard()
-myMiniArm   = QArmMini(hardware=1, id=5)
-kbdNav      = QArmMiniKeyboardNavigator(keyboard=kbd, initialPose=myMiniArm.HOME_POSE)
-myArmMath   = QArmMiniFunctions()
-timer       = QTimer(sampleRate=30.0, totalTime=300.0)
-clawPosition = 0
-edge = 0
 
-try:
-    # main loop
-    while timer.check() and not kbd.states[kbd.K_ESC]:
-        kbd.update() 
-       
-        ##claw movement
-        if kbd.states[kbd.K_LEFT]:
-            clawPosition = min(clawPosition + (1/60), 1)
-        if kbd.states[kbd.K_RIGHT]:
-            clawPosition = max(0, clawPosition - (1/60)) 
 
-        ## Section C - QArm Mini hardware I/O
-        myMiniArm.read_write_std(
-            kbdNav.move_joints_with_keyboard(timer.get_sample_time(), speed=np.pi/4), clawPosition)
+def Stack(height, arm = QArmMini(hardware=1, id=5), max = 5):
+    if height > max:
+        print ("invalid height")
+    else:
+        kbd         = QKeyboard()
+        groundPos   = np.array([0, np.pi/20, -np.pi/4, np.pi*19/20], dtype=np.float64)
+        kbdNav      = QArmMiniKeyboardNavigator(keyboard=kbd, initialPose=arm.HOME_POSE)
+        myArmMath   = QArmMiniFunctions()
+        timer       = QTimer(sampleRate=30.0, totalTime=300.0)
+        clawPosition = 0
+        edge = 0
 
-        ## Section D - Record data for plotting
-        pose, rotationMatrix, gamma = myArmMath.forward_kinematics(myMiniArm.positionMeasured)
-        if kbd.states[kbd.K_SPACE]:
-            if not edge:
-                edge = 1
-        else:
-            edge = 0
+            # main loop
+        while timer.check() and not kbd.states[kbd.K_ESC]:
+            kbd.update() 
         
+            ##claw movement
+            if kbd.states[kbd.K_LEFT]:
+                clawPosition = 0
+            if kbd.states[kbd.K_RIGHT]:
+                clawPosition = 0.5333333333
 
-        timer.sleep()
+            ## Section C - QArm Mini hardware I/O
+            arm.read_write_std(
+                kbdNav.move_joints_with_keyboard(timer.get_sample_time(), speed=np.pi/4), clawPosition)
 
-except KeyboardInterrupt:
-    print('Received user terminate command.')
+            ## Section D - Record data for plotting
+            pose, rotationMatrix, gamma = myArmMath.forward_kinematics(arm.positionMeasured)
+            if kbd.states[kbd.K_SPACE]:
+                if not edge:
+                    edge = 1
+                    print("pose = "+str(pose)+"\nrotationMatrix = "+str(rotationMatrix) + "\ngamma = "+str(gamma))
+            else:
+                edge = 0
+            
 
-finally:
+            timer.sleep()
 
-    # terminate devices
-    myMiniArm.terminate()
+
+for i in range(1):
+    #input()
+    Stack(i+1)
+
+
+
+
+
+
+
+#try:
+#    # main loop
+#    while timer.check() and not kbd.states[kbd.K_ESC]:
+#        kbd.update() 
+#       
+#        ##claw movement
+#        if kbd.states[kbd.K_LEFT]:
+#            clawPosition = min(clawPosition + (1/60), 1)
+#        if kbd.states[kbd.K_RIGHT]:
+#            clawPosition = max(0, clawPosition - (1/60))
+#
+#        ## Section C - QArm Mini hardware I/O
+#        myMiniArm.read_write_std(
+#            kbdNav.move_joints_with_keyboard(timer.get_sample_time(), speed=np.pi/4), clawPosition)
+#
+#        ## Section D - Record data for plotting
+#        pose, rotationMatrix, gamma = myArmMath.forward_kinematics(myMiniArm.positionMeasured)
+#        if kbd.states[kbd.K_SPACE]:
+#            if not edge:
+#                edge = 1
+#        else:
+#            edge = 0
+#        
+#
+#        timer.sleep()
+
+#except KeyboardInterrupt:
+#    print('Received user terminate command.')
+#
+#finally:
+#
+#     #terminate devices
+#    myMiniArm.terminate()
