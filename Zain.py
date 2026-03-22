e1bbfdae0c57da613c9acb1c73f6ec781cd113ff
Zain <zain.ahmed-4@student.manchester.ac.uk>
2026-03-22 12:49:27 +0000

    standard stack

--- Zain.py
+++ Zain.py
@@ -6,9 +6,10 @@ from pal.utilities.keyboard import QKeyboard
 from pal.utilities.timing   import QTimer
 import time
 
-def PickUp(Arm, currentPos, Height = 0, theta = np.pi /13,  ground = np.array([0, np.pi/13,   -np.pi/3 , np.pi/2], dtype=np.float64), angle = -np.pi/2):
-    Arm.read_write_std(currentPos, gripper = 0.5333333333333)
-    time.sleep(1.5)
+def PickUp(Arm, currentPos = None, Height = 0, theta = np.pi /13,  ground = np.array([0, np.pi/13,   -np.pi/3 , np.pi/2], dtype=np.float64), angle = -np.pi/2):
+    if currentPos != None:
+        Arm.read_write_std(currentPos, gripper = 0.5333333333333)
+        time.sleep(1.5)
     Arm.read_write_std(ground, gripper=0.5333333333333)
     time.sleep(1.5)
     Arm.read_write_std(ground, gripper=0)
@@ -31,6 +32,10 @@ def Place(Arm, angle = -np.pi/2, theta = np.pi/13, height = 0):
     Arm.read_write_std(np.array(above), gripper=0)
     time.sleep(1.5)
 
+def standardPlace(arm, height):
+    heightAngles = [np.pi/13, 0.48207, 0.68949, 0.75291]
+    PickUp(arm, Height = height, theta= heightAngles[height])
+    return height + 1
 
 def Manual(position, keyboard, speed = 0.03):
     state = keyboard.states
@@ -49,12 +54,16 @@ def Main():
     myArmMath   = QArmMiniFunctions()
     timer       = QTimer(sampleRate=30.0, totalTime=300.0)
     kbdNav      = QArmMiniKeyboardNavigator(keyboard=kbd, initialPose=mainArm.HOME_POSE)
+    kbdNav      = QArmMiniKeyboardNavigator(keyboard=kbd, initialPose=mainArm.HOME_POSE)
     edge = 0
 
         # main loop
     while timer.check() and not kbd.states[kbd.K_ESC]:
         kbd.update() 
-    
+        
+        mainArm.read_write_std(
+            kbdNav.move_joints_with_keyboard(timer.get_sample_time(), speed=np.pi/4), 0)
+
 
         ## Section D - Record data for plotting
         pose, rotationMatrix, gamma = myArmMath.forward_kinematics(mainArm.positionMeasured)
