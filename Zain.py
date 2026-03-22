979bac2fecedf1eb41712c38654eaf1306da56a2
Zain <zain.ahmed-4@student.manchester.ac.uk>
2026-03-22 03:49:27 +0000

    3 Block stackage

--- Zain.py
+++ Zain.py
@@ -6,15 +6,31 @@ from pal.utilities.keyboard import QKeyboard
 from pal.utilities.timing   import QTimer
 import time
 
-def PickUp(Arm, currentPos):
+def PickUp(Arm, currentPos, Height = 0, theta = np.pi /13,  ground = np.array([0, np.pi/13,   -np.pi/3 , np.pi/2], dtype=np.float64)):
     Arm.read_write_std(currentPos, gripper = 0.5333333333333)
     time.sleep(2)
-    Arm.read_write_std(np.array([0, np.pi/13, -np.pi/2, 8*np.pi/9], dtype=np.float64), gripper=0.5333333333333)
+    Arm.read_write_std(ground, gripper=0.5333333333333)
     time.sleep(2)
-    Arm.read_write_std(np.array([0, np.pi/13, -np.pi/2, 8*np.pi/9], dtype=np.float64), gripper=0)
+    Arm.read_write_std(ground, gripper=0)
     time.sleep(2)
-    Arm.read_write_std(np.array([0, np.pi/13, -np.pi/2, 8*np.pi/9], dtype=np.float64), gripper=0.5333333333333)
+    Arm.read_write_std(ground, gripper=0.5333333333333)
     time.sleep(2)
+    Place(Arm, height = Height, theta= theta)
+
+def Place(Arm, angle = -np.pi/2, theta = np.pi/13, height = 0):
+    phi = (theta + (np.pi/2) - np.arccos(1.1097*np.sin(theta) + 0.456  - (height * 0.403)))
+    arr = [angle,theta,-phi,(2  *np.pi/2)-theta-   phi]  
+    above = [angle, np.pi/2, -phi, np.pi/2-theta+phi]
+    position = np.array(arr, dtype=np.float64)
+    Arm.read_write_std(above, gripper=0.5333333333333)
+    time.sleep(2)
+    Arm.read_write_std(position, gripper=0.5333333333333)
+    time.sleep(2)
+    Arm.read_write_std(position, gripper=0.4)
+    time.sleep(2)
+    Arm.read_write_std(np.array(above), gripper=0)
+    time.sleep(2)
+
 
 def Manual(position, keyboard, speed = 0.03):
     state = keyboard.states
@@ -27,12 +43,12 @@ def Manual(position, keyboard, speed = 0.03):
 
 def Main():
     mainArm = QArmMini(hardware=1, id=5)
-    position = mainArm.HOME_POSE
     height = 0
+    heightAngles = [np.pi/13, 0.48207, 0.68949, 0.75291]
     kbd         = QKeyboard()
-    groundPos   = np.array([0, np.pi/13, -np.pi/2, 5*np.pi/6], dtype=np.float64)
     myArmMath   = QArmMiniFunctions()
     timer       = QTimer(sampleRate=30.0, totalTime=300.0)
+    kbdNav      = QArmMiniKeyboardNavigator(keyboard=kbd, initialPose=mainArm.HOME_POSE)
     clawPosition = 0
     edge = 0
 
@@ -49,17 +65,18 @@ def Main():
         ## Section C - QArm Mini hardware I/O
         #position = Manual(position, kbd)
         mainArm.read_write_std(
-            position, clawPosition)
+            kbdNav.move_joints_with_keyboard(timer.get_sample_time(), speed=np.pi/4), clawPosition)
 
         ## Section D - Record data for plotting
         pose, rotationMatrix, gamma = myArmMath.forward_kinematics(mainArm.positionMeasured)
         if kbd.states[kbd.K_SPACE]:
             if not edge:
                 edge = 1
-                position = Manual(position, kbd)
-                mainArm.read_write_std(position, clawPosition)
+                #position = Manual(position, kbd)  
+                #mainArm.read_write_std(position, clawPosition)
                 #height = (height + 1)%3
-                #PickUp(mainArm, position)
+                PickUp(mainArm, kbdNav.move_joints_with_keyboard(timer.get_sample_time(), speed=np.pi/4), Height = height, theta= heightAngles[height])
+                height = height + 1
                 #print("pose = "+str(pose)+"\nrotationMatrix = "+str(rotationMatrix) + "\ngamma = "+str(gamma))
         else:
             edge = 0
@@ -69,4 +86,3 @@ def Main():
 
 
 Main()
-3
